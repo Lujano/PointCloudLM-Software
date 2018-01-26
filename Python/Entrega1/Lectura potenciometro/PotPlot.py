382ea61904d5db744726d8929b9396c07baeae49
lujano97 <lugaluchinchilla@gmail.com>
2018-01-26 04:58:36 -0400

    Entrega 1, comienzo de control de servos

---
+++ Python/Entrega1/Lectura potenciometro/PotPlot.py
@@ -0,0 +1,55 @@
+import serial
+import matplotlib.pyplot as plt
+
+
+def open_port():
+    ser = serial.Serial('COM12', 115200)
+
+    return ser
+
+def start_protocol(port):
+    c = port.read(2)
+    try:
+        for m in c:
+            print(ord(m))
+
+    except:
+        print("hola")
+
+def close_port(port):
+    port.close()
+
+def detect_data(port):
+    #port.reset_input_buffer()
+    flag = True
+
+    while flag:
+        data_in = port.read(2)
+        if ord(data_in[0]) == 255 and ord(data_in[1]) == 255:
+            flag = False
+
+
+
+
+
+
+
+def main():
+    plt.axis('auto')
+    plt.ylim(0, 5)
+    plt.ion()
+    port = open_port()
+    i = 0.00
+    y = 0.00
+    while(True):
+        detect_data(port)
+        data_in = port.read(2)
+        port_value = ord(data_in[0])+(2**8)*ord(data_in[1])
+        i += 1
+        y = port_value*3.2/(2**12-1)
+        #plt.scatter(i, y)
+        print(y)
+        plt.pause(0.000005)
+
+
+if __name__ == "__main__": main()
--- Python/Entrega1/PruebaServos/Servo.py
+++ Python/Entrega1/PruebaServos/Servo.py
@@ -6,31 +6,38 @@ def open_port():
 
     return ser
 
-def start_protocol(port):
-    c = port.read(2)
-    try:
-        for m in c:
-            print(ord(m))
-
-    except:
-        print("hola")
 
 def close_port(port):
     port.close()
 
 
+def servo1_send(port, posicion): # el servo que controla phi (plano xy)
+    direccion = 0 # conector del driver al que esta conectado el motor
+    port.write(bytes(255, direccion, posicion))
 
-
+def servo2_send(port, posicion): # el servo que controla theta (respecto al eje z)
+    direccion = 1 # conector del driver al que esta conectado el motor
+    port.write(bytes(255, direccion, posicion))
 
 def main():
 
-   # port = open_port()
-    i = 0.00
-    y = 0.00
-    while(True):
-        i += 1
-        print(i)
-        time.sleep(0.02) # delay en segundos
+    port = open_port()
+    i = 0
+    for step1 in range (0, 254):
+        servo1_send(port, step1)
+        if i == 0:
+            for step2 in range(100, 254, 2 ):
+                servo2_send(port, step2)
+                time.sleep(0.03) # delay en segundos
+            i == 1
+        else:
+            for step2 in range(253, 100, -2 ):
+                servo2_send(port, step2)
+                time.sleep(0.03) # delay en segundos
+            i == 0
+
+    close_port(port)
+
 
 
 if __name__ == "__main__": main()
