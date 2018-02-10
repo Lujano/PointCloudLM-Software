5b1111f059e0d31684ba6d79b67b5fa834af3f25
lujano97 <lugaluchinchilla@gmail.com>
2018-02-10 13:27:15 -0400

    Codigo de adquisicion de arduino Funcionando
    Co-Authored-By: rjmata95 <rjmata95@users.noreply.github.com>

--- Adquisicion/AdquisicionARDUINO/Adquisision_ARD_Fase1.py
+++ Adquisicion/AdquisicionARDUINO/Adquisision_ARD_Fase1.py
@@ -1,6 +1,7 @@
 import serial
 import matplotlib.pyplot as plt
-
+import numpy as np
+import time
 
 def open_port():
     ser = serial.Serial('COM27', 115200)
@@ -18,7 +19,7 @@ def detect_data(port):
         anuncio = port.read(1)
         anuncio = ord(anuncio[0]) # convertir en entero
 
-        if (anuncio & 0xf0)  == 0xf0 # Se detecta el byte de anuncio de trama
+        if (anuncio & 0xf0)  == 0xf0: # Se detecta el byte de anuncio de trama
                 n_canales = anuncio & 0x0f # Numero de canales a leer
                 return  n_canales
 
@@ -35,15 +36,20 @@ def main():
         n_canales = detect_data(port)
         data_in = port.read(2*n_canales)
         canal_n1 = (2**7)*ord(data_in[0])+ord(data_in[1])
+        echo = (2**15)*ord(data_in[2])+(2**8)*ord(data_in[3])+(2**7)*ord(data_in[4])+ ord(data_in[5])
+        step1 = (2**7)*ord(data_in[6])+ord(data_in[7])
+        step2 = (2**7)*ord(data_in[8])+ord(data_in[9])
         y = canal_n1*3.2/(2**12-1) # Escalamiento
         i += 1
-        Amplitud_matrix = np.append(Amplitud_matrix, [y])
-        Time_matrix = np.append(Time_matrix, [i])
+        #Amplitud_matrix = np.append(Amplitud_matrix, [y])
+        #Time_matrix = np.append(Time_matrix, [i])
         T_Final = time.time()
         Dif = T_Final - T_Inicio
+        print("echo = {}, step1 = {}, step2 = {}".format(echo/58.0, step1, step2))
 
-    plt.plot(Time_matrix, Amplitud_matrix)
-    plt.show()
+    close_port(port)
+    # plt.plot(Time_matrix, Amplitud_matrix)
+    # plt.show()
 
 
 if __name__ == "__main__": main()
